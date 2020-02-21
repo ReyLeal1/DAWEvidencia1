1a6178ac1f81373fd0fa84a8a908ce47a7b56bb4
Rey Leal <david.reach9911@gmail.com>
2020-02-21 01:03:52 -0700

    Carrito de compra

---
+++ DAWEvidencia1/src/java/Servlets/Carrito.java
@@ -0,0 +1,114 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Servlets;
+
+import Java.Libro;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.List;
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ *
+ * @author reyleal
+ */
+public class Carrito extends HttpServlet {
+
+    /**
+     * Processes requests for both HTTP <code>GET</code> and <code>POST</code>
+     * methods.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    protected void processRequest(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        response.setContentType("text/html;charset=UTF-8");
+        try (PrintWriter out = response.getWriter()) {
+            /* TODO output your page here. You may use following sample code. */
+            out.println("<!DOCTYPE html>");
+            out.println("<html>");
+            out.println("<head>");
+            out.println("<title>Servlet Carrito</title>");            
+            out.println("</head>");
+            out.println("<body>");
+            out.println("<h1>Servlet Carrito at " + request.getContextPath() + "</h1>");
+            out.println("</body>");
+            out.println("</html>");
+        }
+    }
+
+    // <editor-fold defaultstate="collapsed" desc="HttpServlet methods. Click on the + sign on the left to edit the code.">
+    /**
+     * Handles the HTTP <code>GET</code> method.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        RequestDispatcher view = 
+            request.getRequestDispatcher("Cobro.jsp");
+            view.forward(request, response);
+    }
+
+    /**
+     * Handles the HTTP <code>POST</code> method.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        
+        if(request.getAttribute("nombre") != null) {
+            List<Libro> libros = (List) getServletContext().getAttribute("libros");
+        
+            Libro libro = null;
+            String nombre = request.getParameter("nombre");
+            for(Libro l : libros) {
+                if(l.getNombreLibro().equals(nombre)) {
+                    libro = l;
+                }
+            }
+
+            if(libro != null) {
+                request.setAttribute("libro", libro);
+
+                RequestDispatcher view = 
+                    request.getRequestDispatcher("Carrito.jsp");
+                view.forward(request, response);
+            }
+        } else {
+            RequestDispatcher view = 
+                request.getRequestDispatcher("inicio.jsp");
+            view.forward(request, response);
+        }
+    }
+
+    /**
+     * Returns a short description of the servlet.
+     *
+     * @return a String containing servlet description
+     */
+    @Override
+    public String getServletInfo() {
+        return "Short description";
+    }// </editor-fold>
+
+}
