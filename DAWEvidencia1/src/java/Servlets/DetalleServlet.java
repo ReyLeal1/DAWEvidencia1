ffd202f333390313158dca329b9bafcb605d6725
Rey Leal <david.reach9911@gmail.com>
2020-02-21 01:03:01 -0700

    Imagen para el carrito

--- DAWEvidencia1/src/java/Servlets/DetalleServlet.java
+++ DAWEvidencia1/src/java/Servlets/DetalleServlet.java
@@ -1,8 +1,4 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package Servlets;
 
 import Java.Libro;
@@ -19,43 +15,41 @@ import javax.servlet.http.HttpServletResponse;
  * @author Brandon
  */
 public class DetalleServlet extends HttpServlet {
+    private Libro libro = null;
 
-    /**
-     * Handles the HTTP <code>POST</code> method.
-     *
-     * @param request servlet request
-     * @param response servlet response
-     * @throws ServletException if a servlet-specific error occurs
-     * @throws IOException if an I/O error occurs
-     */
     @Override
     protected void doPost(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
         
-        List<Libro> libros = (List) getServletContext().getAttribute("libros");
-        
-        Libro libro = null;
-        String nombre = request.getParameter("nombre");
-        for(Libro l : libros) {
-            if(l.getNombreLibro().equals(nombre)) {
-                libro = l;
+        if(request.getParameter("comprar") != null) {
+                request.setAttribute("libro", libro);
+                // Redirect to index
+                RequestDispatcher view =
+                        request.getRequestDispatcher("Carrito.jsp");
+                view.forward(request, response);
             }
-        }
-        
-        if(libro != null) {
-            request.setAttribute("libro", libro);
-            // Redirect to index
-            RequestDispatcher view =
-                    request.getRequestDispatcher("detalle.jsp");
-            view.forward(request, response);
+        else if(getServletContext().getAttribute("libros") != null) {
+            List<Libro> libros = (List) getServletContext().getAttribute("libros");
+
+            
+            String nombre = request.getParameter("nombre");
+            for(Libro l : libros) {
+                if(l.getNombreLibro().equals(nombre)) {
+                    libro = l;
+                }
+            }
+
+            if(libro != null) {
+                request.setAttribute("libro", libro);
+                // Redirect to index
+                RequestDispatcher view =
+                        request.getRequestDispatcher("detalle.jsp");
+                view.forward(request, response);
+            }
         }
     }
 
-    /**
-     * Returns a short description of the servlet.
-     *
-     * @return a String containing servlet description
-     */
+
     @Override
     public String getServletInfo() {
         return "Short description";
